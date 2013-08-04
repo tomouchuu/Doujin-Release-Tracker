6e77c4599273a9d116bfc5d76cb8340bf9a3bb5b
Thomas Moore <tsanm93@gmail.com>
2013-08-04 23:08:53 +0100

    Added Attendee List (People selling at Comiket but not sure what)

---
+++ js/attending.js
@@ -0,0 +1,39 @@
+var attending;
+
+attending = {
+    "attending": [
+        {
+            "artist": "DECO*27"
+        },
+        {
+            "artist": "sasakure.UK"
+        },
+        {
+            "artist": "40mP"
+        },
+        {
+            "artist": "HoneyWorks"
+        },
+        {
+            "artist": "wowaka"
+        },
+        {
+            "artist": "KEI (Homing Echo)"
+        },
+        {
+            "artist": "8#Prince"
+        },
+        {
+            "artist": "baker"
+        },
+        {
+            "artist": "Dixie Flatline"
+        },
+        {
+            "artist": "AVTechNO!"
+        },
+        {
+            "artist": "buzzG"
+        }
+    ]
+};
