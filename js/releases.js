8283683cddc80688b772a4dd119ea5795b2af885
Thomas Moore <tsanm93@gmail.com>
2013-08-04 21:55:04 +0100

    Using Handlebars. Page just needs more styling.

---
+++ js/releases.js
@@ -0,0 +1,18 @@
+var releases;
+
+releases = {
+    "releases": [
+        {
+            "album": "戯言クライン",
+            "artist": "Kano",
+            "link": "http://www.nicovideo.jp/watch/sm21502977",
+            "available": false
+        },
+        {
+            "album": "柿チョコ×びびあん",
+            "artist": "EGOIST II",
+            "link": "http://www.nicovideo.jp/watch/sm21514574",
+            "available": false
+        }
+    ]
+};
