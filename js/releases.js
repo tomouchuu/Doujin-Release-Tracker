d2c50b8145c5cbfad4dcdc7178f1479513b3e522
Thomas Moore <tsanm93@gmail.com>
2013-08-07 15:18:46 +0100

    Added type option for sorting on the homepage.

--- js/releases.js
+++ js/releases.js
@@ -6,66 +6,77 @@ releases = {
             "album": "Style",
             "artist": "Last Note.",
             "link": "http://www.nicovideo.jp/watch/sm21513134",
+            "type": "utaite",
             "available": false
         },
         {
             "album": "Stance on Wave",
             "artist": "Cametek",
             "link": "http://www.nicovideo.jp/watch/sm21514719",
+            "type": "vocaloid",
             "available": false
         },
         {
             "album": "Caramel",
             "artist": "シャノ × 40mP",
             "link": "http://www.nicovideo.jp/watch/sm21522889",
+            "type": "utaite",
             "available": false
         },
         {
             "album": "戯言クライン",
             "artist": "Kano",
             "link": "http://www.nicovideo.jp/watch/sm21502977",
+            "type": "utaite",
             "available": false
         },
         {
             "album": "EGOIST II",
             "artist": "柿チョコ×びびあん",
             "link": "http://www.nicovideo.jp/watch/sm21514574",
+            "type": "utaite",
             "available": false
         },
         {
             "album": "[i:d]",
             "artist": "sasakure.UK",
             "link": "http://www.nicovideo.jp/watch/sm21529595",
+            "type": "vocaloid",
             "available": false
         },
         {
             "album": "虹色ショコラティエ",
             "artist": "ゆいこんぬ",
             "link": "http://www.nicovideo.jp/watch/sm21521305",
+            "type": "utaite",
             "available": false
         },
         {
             "album": "PON＊",
             "artist": "あやぽんず＊",
             "link": "http://www.nicovideo.jp/watch/sm21513960",
+            "type": "utaite",
             "available": false
         },
         {
             "album": "Reverberations",
             "artist": "Clean Tears (S.C.X.)",
             "link": "https://soundcloud.com/cleantears/reverberations-preview",
+            "type": "vocaloid",
             "available": false
         },
         {
             "album": "Rhapsody Notes",
             "artist": "綿飴（季節P）",
             "link": "http://www.nicovideo.jp/watch/sm21467054",
+            "type": "vocaloid",
             "available": false
         },
         {
             "album": "colorful x peace",
             "artist": "mukami",
             "link": "http://www.nicovideo.jp/watch/sm21478333",
+            "type": "vocaloid",
             "available": false
         },
     ]
