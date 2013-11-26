817128c3fe3191caa5246b6744bd9411b4b7038a
Thomas Moore <tom@ahead4.com>
2013-11-26 17:29:13 +0000

    Start of the route checker to stop bad archive links

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,6 +7,20 @@ angular.module('comiketApp')
     {
       $scope.comiketID = 'c85';
     }
+    
+    var events = $resource('/events/events.json');
+    $scope.events = events.get();
+    var validEvent = false;
+
+    // Loop through the events and compare the $scope.comiketID with events.shortname
+    // When there is a match set validEvent to true
+
+    if (validEvent === false)
+    {
+      // This needs to redirect to C85 rather than point to C85
+      $scope.comiketID = 'c85';
+    }
+    
     $scope.releasesURL = '/events/:comiketID.json';
 
     var releases = $resource($scope.releasesURL, {comiketID: '@cID'});
