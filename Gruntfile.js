2b53e7ce110db9c0a2952a290379cebdedd613c5
Thomas Moore <tsanm93@gmail.com>
2013-11-26 01:58:15 +0000

    Fix Image Not Loading and Added a Blank Release to C85

--- Gruntfile.js
+++ Gruntfile.js
@@ -168,7 +168,7 @@ module.exports = function (grunt) {
           src: [
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
-            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+            // '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
             '<%= yeoman.dist %>/styles/fonts/*'
           ]
         }
