b790bd8f0aa0a3dd1e1ed9a13524fab23704bbb2
Thomas Moore <tsanm93@gmail.com>
2013-11-28 20:09:51 +0000

    Fixed the archive routes.
    Could do with a redirect, and also a message to say why it redirected.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,19 +1,23 @@
 'use strict';
 
 angular.module('comiketApp')
-  .controller('MainCtrl', function ($scope, $routeParams, $resource) {
+  .controller('MainCtrl', function ($scope, $routeParams, $q, $http) {
     $scope.comiketID = $routeParams.comiketID;
     if (angular.isDefined($scope.comiketID) === false)
     {
       $scope.comiketID = 'c85';
     }
     
-    var releasesURL = '/events/:comiketID.json';
+    var releasesURL = '/events/'+$scope.comiketID+'.json';
 
-    // Use $http to grab releases (http://docs.angularjs.org/api/ng.$http) then use the error callback when it can't find the Comiket ID to redirect to c85
-
-    // var releases = $resource($scope.releasesURL, {comiketID: '@cID'});
-    // var release = releases.get({comiketID: $scope.comiketID}, function() {
-    //   $scope.releases = release;
-    // });
+    $http({method: 'GET', url: releasesURL}).
+      success(function(data,status){
+        $scope.status = status;
+        $scope.data = data;
+      }).
+      error(function(data,status){
+        $scope.comiketID = 'c85';
+        $scope.status = status;
+        $scope.data = data || 'Request Failed';
+      });
   });
