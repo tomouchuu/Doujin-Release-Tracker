a2200e0fb193ed3bc38dc76233a5e44da2ca9dab
Thomas Moore <tsanm93@gmail.com>
2014-04-11 03:45:24 +0900

    Wiping for New Version

--- app/scripts/controllers/archives.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('comiketApp')
-  .controller('ArchivesCtrl', function ($scope, $resource) {
-    var events = $resource('/events/events.json');
-    $scope.events = events.get();
-  });
--- app/scripts/controllers/contrib.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('comiketApp')
-  .controller('ContribCtrl', function ($scope, $resource) {
-    var contributors = $resource('/contributors.json');
-    $scope.contributors = contributors.get();
-    $scope.test = 'Test Message';
-  });
