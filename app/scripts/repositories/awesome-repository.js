1309d5d6f014ba0b06cbd62e5e82903cffefe5bb
Thomas Moore <tsanm93@gmail.com>
2014-04-20 12:09:58 +0900

    Removed 'awesome' example things

--- app/scripts/repositories/awesome-repository.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('comiket')
-  .factory('AwesomeRepository', function ($injector, AwesomeModel) {
-    var BaseRepository = $injector.get('BaseRepository');
-    return new BaseRepository({name: 'AwesomeRepository', model: AwesomeModel});
-  });
