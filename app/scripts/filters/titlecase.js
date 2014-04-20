293000ac0c1c9bd44f322272407e85c5008b297d
Thomas Moore <tsanm93@gmail.com>
2014-04-21 07:58:41 +0900

    Simpler titlecase

--- app/scripts/filters/titlecase.js
+++ app/scripts/filters/titlecase.js
@@ -1,13 +1,11 @@
 'use strict';
 
 angular.module('doujinReleaseTracker')
-  .filter('titlecase', function () {
-    return function (input) {
-      var words = input.split(' ');
-      for (var i = 0; i < words.length; i++) {
-        words[i] = words[i].toLowerCase(); // lowercase everything to get rid of weird casing issues
-        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
-      }
-      return words.join(' ');
-    }
-  });
+    .filter('titlecase', function () {
+        return function (input) {
+            if (input)
+            {
+              return input.charAt(0).toUpperCase() + input.slice(1);
+            }
+        };
+    });
