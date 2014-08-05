b56b1e541055a6360587c8405b4515187fc7fdce
Thomas Moore <tsanm93@gmail.com>
2014-08-05 23:16:32 +0100

    C86 LIVE!! Currently at 294 Releases!
    Site live here: http://comiket.doujinreleas.es/

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('doujinReleaseTracker')
-  .config(function ($urlRouterProvider) {
+  .config(function ($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.when('', '/');
     $urlRouterProvider.otherwise("/error?code=404");
+
+    $locationProvider.html5Mode(true).hashPrefix('!');
   });
